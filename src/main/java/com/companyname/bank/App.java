815d7232dc722cbe01af0d5f2b2af16d3f817b38
Anil kumar <121420630+anildev1@users.noreply.github.com>
2023-04-15 15:31:06 +0530

    SCM POLLING

--- src/main/java/com/companyname/bank/App.java
+++ src/main/java/com/companyname/bank/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "new changes made  ");
+        System.out.println( "new changes made by SCM POLLING  ");
     }
 }
